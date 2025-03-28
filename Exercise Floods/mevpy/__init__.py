bdb5136962b646a0a1c4e4af7c6b794feeed2cd8
matteocarrari <matteo.carrari60@gmail.com>
2025-03-28 17:59:26 +0100

    Add files via upload

---
+++ Exercise Floods/mevpy/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import division # necessary for Python 2
+'''
+This is the mev_package
+'''
+
+if __name__ == "__main__":
+    print("mev package run as main")
+
+#  = ["gev_functions", "mev_functions"]
+
+# from gev_functions import *
+# from .myfun import *
+from .gev_fun import *
+from .mev_fun import *
+
+
